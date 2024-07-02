b8e778f6ddde005c56901fcb60a3f9c5b33beeb9
Maciej Dudzik <maciejdudzik90@gmail.com>
2024-07-03 01:16:52 +0200

    adding new files

--- repository/test_auto/main.py
+++ repository/test_auto/main.py
@@ -1,14 +1,12 @@
 def factorials(n):
-    factorial = 1
-    for i in range(1, n + 1):
-        factorial *= i
-        yield factorial
-
-
-def main():
-    for i, factorial in enumerate(factorials(100)):
-        print(f"{i + 1}! = {factorial}")
-
-
-if __name__ == "__main__":
-    main()
+    if n < 0:
+        return None
+    elif n == 0:
+        return 1
+    elif isinstance(n, int):
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+    else:
+        raise TypeError("Only integer values are allowed.")
---
+++ repository/test_auto/tests/test_factorials.py
@@ -0,0 +1,13 @@
+import pytest
+from main import factorials
+
+
+def test_factorials():
+    assert factorials(-1) is None
+    assert factorials(0) == 1
+    assert factorials(5) == 120
+
+
+def test_factorials_invalid_input():
+    with pytest.raises(TypeError):
+        factorials(2.5)
